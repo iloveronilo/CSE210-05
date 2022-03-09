7cdc1eebd8a29a803a78a04d15d35889bae7a64b
jasnatbyui <jasnatbyui@gmail.com>
2022-03-09 22:03:08 +1300

    Scripting changes to suit new classes

---
+++ scripting/draw_actors_action.py
@@ -0,0 +1,44 @@
+from game.scripting.action import Action
+
+
+class DrawActorsAction(Action):
+    """
+    An output action that draws all the actors.
+    
+    The responsibility of DrawActorsAction is to draw all the actors.
+
+    Attributes:
+        _video_service (VideoService): An instance of VideoService.
+    """
+
+    def __init__(self, video_service):
+        """Constructs a new DrawActorsAction using the specified VideoService.
+        
+        Args:
+            video_service (VideoService): An instance of VideoService.
+        """
+        self._video_service = video_service
+
+    def execute(self, cast, script):
+        """Executes the draw actors action.
+
+        Args:
+            cast (Cast): The cast of Actors in the game.
+            script (Script): The script of Actions in the game.
+        """
+        score1 = cast.get_first_actor("scores1")
+        score2 = cast.get_first_actor("scores2")
+        trail = cast.get_first_actor("trails")
+        snake1 = cast.get_first_actor("snakes1")
+        snake2 = cast.get_first_actor("snakes2")
+        segments = snake1.get_segments()
+        messages = cast.get_actors("messages")
+
+        self._video_service.clear_buffer()
+        self._video_service.draw_actor(trail)
+        self._video_service.draw_actors(segments)
+        self._video_service.draw_actor(score1)
+        self._video_service.draw_actor(score2)
+        self._video_service.draw_actor(snake2)
+        self._video_service.draw_actors(messages, True)
+        self._video_service.flush_buffer()
